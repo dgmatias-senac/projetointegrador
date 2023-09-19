746fe5bfc24bc4bc21d13cc5cf675142118812d1
dgmatias-senac <145495645+dgmatias-senac@users.noreply.github.com>
2023-09-19 19:17:12 -0300

    Add files via upload

---
+++ projetointegrador-senac-main/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CarrosselComponent } from './home/carrossel/carrossel.component';
+import { DestaqueComponent } from './home/carrossel/destaque/destaque.component';
+import { HeaderComponent } from './home/carrossel/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CarrosselComponent,
+    DestaqueComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
